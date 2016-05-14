01c5cc41a51878decfbb7cbdb76d2448fc9603ef
Paul Gluchow <paul.gluchow@stud.fh-rosenheim.de>
2016-05-14 23:58:47 +0200

    initial

---
+++ Memories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memories
+//
+//  Created by admin on 11.05.16.
+//  Copyright © 2016 gluglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
